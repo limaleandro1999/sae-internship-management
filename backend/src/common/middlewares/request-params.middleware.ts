0bc321e355dff76afd48d124bd37dd418f61a4d3
Leandro lima <leandro.lima@oowlish.com>
2021-01-19 22:19:59 -0300

    changing request params middleware to accept null values for filters and sort params

--- backend/src/common/middlewares/request-params.middleware.ts
+++ backend/src/common/middlewares/request-params.middleware.ts
@@ -12,10 +12,10 @@ export class RequestParamsMiddleware implements NestMiddleware {
   
     req.queryInfo = {};
 
-    req.queryInfo.skip = range ? range[0] : null;
+    req.queryInfo.skip = range ? range[0] : 0;
     req.queryInfo.take = range ? range[1] - range[0]: null;
-    req.queryInfo.order = sort ? { [sort[0]]: sort[1] } : null;
-    req.queryInfo.filter = filter ? JSON.parse(<string>filter) : null;
+    req.queryInfo.order = sort ? { [sort[0]]: sort[1] } : { id: 'ASC' };
+    req.queryInfo.filter = filter ? JSON.parse(<string>filter) : { q: null };
     
     next();
   }
