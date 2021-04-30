beffb76ee708533770191c5c69d9a8d2c70df408
Leandro lima <leandro.lima@oowlish.com>
2021-04-29 23:39:19 -0300

    removing comment

--- frontend/src/components/Intern/InternCreate.js
+++ frontend/src/components/Intern/InternCreate.js
@@ -4,8 +4,6 @@ import { Create } from 'react-admin';
 import InternForm from './InternForm';
 
 function InternCreate(props) {
-  // const userType = localStorage.getItem('role');
-
   return (
     <Create {...props} title="Novo Orientador de Estágio">
       <InternForm />
